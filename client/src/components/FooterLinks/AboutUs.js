585f27d4ba5e04fad428253168c46884e9ab50b6
mich-hales <michellehales93@gmail.com>
2022-12-30 12:41:22 -0700

    added avatars to about us page

--- client/src/components/FooterLinks/AboutUs.js
+++ client/src/components/FooterLinks/AboutUs.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import Michelle from '../../assets/images/michelle.png';
+import Noah from '../../assets/images/noah.png';
+import Rexx from '../../assets/images/rexx.png';
+import Allen from '../../assets/images/allen.png';
 
 const AboutUs = () => {
   return (
@@ -12,6 +16,31 @@ const AboutUs = () => {
         <li className='my-5 mb-9'>A messaging system for communicating with friends</li>
       </ul>
       <p className='mx-12 xl:mx-40 lg:mx-40 md:mx-32 sm:mx-20 text-lg text-center my-5'>Our goal is to make it easy for you to connect with others and stay connected, no matter where you are. Thank you for choosing Connect With Me!</p>
+
+      <h1 className='text-center text-2xl bold mx-10 mt-12 text-teal-600'>Meet the Team</h1>
+      <div className='flex justify-center'>
+        <div>
+        <img className='h-52 w-auto' alt='michelle' src={Michelle}></img>
+          <h3 className='text-center mt-3 bold'>Michelle Hales</h3>
+        </div>
+      
+      <div>
+      <img className='h-52 w-auto' alt='noah' src={Noah}></img>
+        <h3 className='text-center mt-3 bold'>Noah Brimhall</h3>
+      </div>
+      
+      <div>
+      <img className='h-52 w-auto' alt='rexx' src={Rexx}></img>
+      <h3 className='text-center mt-3 bold'>Rexx Madsen</h3>
+      </div>
+      
+      <div>
+      <img className='h-52 w-auto' alt='allen' src={Allen}></img>
+      <h3 className='text-center mt-3 bold'>Allen Robson</h3>
+      </div>
+      
+      </div>
+      
     </div>
   );
 };
