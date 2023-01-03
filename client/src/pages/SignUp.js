af52c3e00a545d7968ee42fc09e21a1f9dc9450c
mich-hales <michellehales93@gmail.com>
2023-01-02 20:45:25 -0700

    changed styling on sign up page to be dark mode as well

--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -55,28 +55,28 @@ export default function SignUp() {
   };
 
   return (
-    <section className="bg-gray-50 dark:bg-gray-900">
+    <section className="bg-gray-900">
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-        <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+        <div className="w-full rounded-lg shadow border md:mt-0 sm:max-w-md xl:p-0 bg-gray-800 border-gray-700">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+            <h1 className="text-xl font-bold leading-tight tracking-tight md:text-2xl text-white">
               Sign up for an account
             </h1>
             <form className="space-y-4 md:space-y-6" action="#" onSubmit={handleFormSubmit}>
               <div>
-                <label htmlFor="firstName" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">First Name</label>
+                <label htmlFor="firstName" className="block mb-2 text-sm font-medium text-white">First Name</label>
                 <input
                   value={userFormData.firstName}
                   type="text"
                   name="firstName"
                   id="firstName"
                   onChange={handleInputChange}
-                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  className="border sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
                   placeholder="John"
                   required="" />
               </div>
               <div>
-                <label htmlFor="lastName" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Last Name</label>
+                <label htmlFor="lastName" className="block mb-2 text-sm font-medium text-white">Last Name</label>
                 <input
                   value={userFormData.lastName}
                   type="text"
@@ -112,7 +112,7 @@ export default function SignUp() {
                   required="" />
               </div>
               <div>
-                <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                <label htmlFor="password" className="block mb-2 text-sm font-medium text-white">Password</label>
                 <input
                   value={userFormData.password}
                   type="password"
